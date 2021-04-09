ef347be5718d3eeacc4d367b60018b6c4b3f5b1d
Anton Ilchenko <anton.ilchenko98@gmail.com>
2021-04-09 16:59:00 +0300

    1.Defined html resolver for proceeding html pages. 2.Configured returning of static resources 3.Configured getting index.html for request "/"

--- src/main/java/lab3/gradebook/nc/config/AppConfig.java
+++ src/main/java/lab3/gradebook/nc/config/AppConfig.java
@@ -1,11 +1,36 @@
 package lab3.gradebook.nc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "lab3.gradebook.nc.controllers")
 @EnableWebMvc
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver htmlResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/static/");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/static/", "/WEB-INF/static/css/", "/WEB-INF/static/js/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
--- src/main/java/lab3/gradebook/nc/controllers/MainController.java
+++ src/main/java/lab3/gradebook/nc/controllers/MainController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     @GetMapping("/")
-    @ResponseBody
     public String main() {
-        return "Hello! This is a gradebook!";
+        return "index";
     }
 }
