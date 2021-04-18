d974dc2891b024de0dd3eb88e31b56ae47eae3e8
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-04-18 22:57:13 +0300

    Edit/add operations for subjects&lessons are done

---
+++ src/main/java/lab3/gradebook/nc/controllers/LessonController.java
@@ -0,0 +1,78 @@
+package lab3.gradebook.nc.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lab3.gradebook.nc.controllers.utils.CustomFormatResponseBody;
+import lab3.gradebook.nc.model.db.DAOException;
+import lab3.gradebook.nc.model.db.DaoLesson;
+import lab3.gradebook.nc.model.entities.Lesson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/lessons")
+public class LessonController {
+    private DaoLesson daoLesson;
+    private CustomFormatResponseBody customFormatResponseBody;
+
+    @Autowired
+    public LessonController(DaoLesson daoLesson, CustomFormatResponseBody customFormatResponseBody) {
+        this.daoLesson = daoLesson;
+        this.customFormatResponseBody = customFormatResponseBody;
+    }
+
+    @GetMapping("{id}")
+    @ResponseBody
+    public String getLessonsByID(@PathVariable int id) throws JsonProcessingException, DAOException {
+        Lesson lesson = daoLesson.getById(id);
+        return lesson != null ?
+                customFormatResponseBody.buildResponse(true, lesson) :
+                customFormatResponseBody.buildResponse(
+                        false,
+                        "Cannot find lesson by id");
+    }
+    @DeleteMapping("{id}")
+    @ResponseBody
+    public String delete(@PathVariable int id) throws JsonProcessingException {
+        try {
+            daoLesson.delete(id);
+            return customFormatResponseBody.buildResponse(true, "Subject deleted successfully");
+        } catch (DAOException e) {
+            return customFormatResponseBody.buildResponse(false, "Subject deleted unsuccessfully");
+        }
+    }
+    @PutMapping
+    public ResponseEntity<?> edit(@RequestBody Lesson lesson) throws JsonProcessingException {
+        try {
+            daoLesson.edit(lesson);
+            return ResponseEntity.ok(
+                    customFormatResponseBody.buildResponse(
+                            true,
+                            "Lesson edited successfully"));
+        } catch (DAOException e) {
+            return ResponseEntity.status(400).body(
+                    customFormatResponseBody.buildResponse(
+                            false,
+                            "Lesson edited unsuccessfully"));
+        }
+    }
+
+    @PostMapping
+    @ResponseBody
+    public String add(@RequestBody Lesson lesson) throws JsonProcessingException {
+        try {
+            daoLesson.add(lesson);
+            return customFormatResponseBody.buildResponse(true, "Lesson added successfully");
+        } catch (DAOException e) {
+            return customFormatResponseBody.buildResponse(false, "Lesson added unsuccessfully");
+        }
+    }
+}
--- src/main/java/lab3/gradebook/nc/model/entities/Lesson.java
+++ src/main/java/lab3/gradebook/nc/model/entities/Lesson.java
@@ -3,12 +3,14 @@ package lab3.gradebook.nc.model.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Lesson {
     private int id;
     private String title;
