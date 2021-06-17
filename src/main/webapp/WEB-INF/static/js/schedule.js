1019c1b6b0e35216970719afd5b3bb41798f0a24
Anton Ilchenko <anton.ilchenko98@gmail.com>
2021-06-17 14:31:18 +0300

    Fixed showing color of selected navbar item

--- src/main/webapp/WEB-INF/static/js/schedule.js
+++ src/main/webapp/WEB-INF/static/js/schedule.js
@@ -21,19 +21,22 @@ function getSchedule() {
 }
 function renderSchedule(schedule) {
     $.each(schedule, function(key, value) {
-        let subjectHref;
         let lessonHref = 'onclick="location.href = \'/lessons/' + value.lesson.id+ '\';"';
-        if (value.role === 'TEACHER') {
-            subjectHref = 'onclick="location.href = \'/subjects/' + value.subjectId+ '\';"';
-
-        }
-        if (value.role === 'STUDENT') {
-            subjectHref = 'onclick="location.href = \'/studying/subjects/' + value.subjectId+ '\';"';
-        }
         $('#schedule-table').append('<tr id="subject-' + value.id + '">' +
             '            <th>'+ dateTimeFromLocalDateTime(value.lesson.startDate) +'</th>' +
-            '            <td> <a class="link-secondary" '+ subjectHref + '>' + value.subjectName + '</a></td>' +
+            '            <td> <a class="link-secondary" onclick="renderOnClickOfSubject(' + value.subjectId + ', \'' + value.role+ '\')">' + value.subjectName + '</a></td>' +
             '            <td> <a class="link-secondary" ' + lessonHref + '>' + value.lesson.title + '</a></td>' +
             '        </tr>');
     });
+}
+
+function renderOnClickOfSubject(id, role) {
+    if (role === 'TEACHER') {
+        localStorage.setItem('activeNavItem', 'nav-item-teaching');
+        location.href = '/subjects/' + id;
+    }
+    if (role === 'STUDENT') {
+        localStorage.setItem('activeNavItem', 'nav-item-studying');
+        location.href = '/studying/subjects/' + id;
+    }
 }
